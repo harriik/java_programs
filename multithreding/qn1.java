e11062ad04cf31613c657789d438de63f2f3c5e6
Harikesan D J <143055692+harriik@users.noreply.github.com>
2026-04-16 08:53:00 +0000

    Thread concepts

--- multithreding/qn1.java
+++ multithreding/qn1.java
@@ -1,15 +1,35 @@
-class MyThread extends Thread{
-    public void run(){
-        String text = "Good morning";
-        System.out.println(text);
+class MyThread extends Thread {
+    String name;
+    int delay;
+    public MyThread(String name, int delay) {
+        this.name = name;
+        this.delay = delay;
+    }
+    public void run() {
+        for(int i=0;i<5;i++){
+            try {
+                System.out.println(name);
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-public class qn1 {
 
-    public static void main(String[] args) {
-        MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
+public class qn1{
+    public static void main(String[] args) throws Exception{
+        MyThread t1 = new MyThread("Good morning", 1000);
+        MyThread t2 = new MyThread("Hello", 2000);
+        MyThread t3 = new MyThread("Welcome", 3000);
+
         t1.start();
         t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
     }
 }
